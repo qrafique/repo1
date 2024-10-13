1919b2bf443dbb676e31dfdafcdcef9079c78072
qasim <qrafique@gmail.com>
2024-10-13 16:56:14 +0500

    1st

---
+++ calculator.py
@@ -0,0 +1,8 @@
+num1=int(input("enter no. 1:"))
+num2=int(input("enter no.2: "))
+sum=num1+num2
+print("The sum of num1 & num2 is:",sum)
+diff_num=num1-num2
+print("The difference of num1 and num2 is:", diff_num)
+product_num=num1*num2
+print("The product is:", product_num)
