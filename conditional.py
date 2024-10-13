1adafb8f11fbddc5a958b446e98fc4a12b9c8ef9
qasim <qrafique@gmail.com>
2024-10-13 20:11:58 +0500

    coditional.py day of week added

--- conditional.py
+++ conditional.py
@@ -7,3 +7,10 @@ elif marks>=70:
     print("Grade: C")
 else:
     print("Fail")
+
+day_of_week=input("Enter day of the week:").lower()
+
+if day_of_week =='saturday' or day_of_week=='sunday':
+    print("i will learn devOps live!")
+else:
+    print(" iwill practice devOps!!!!!!")
