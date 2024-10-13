b43d3ab70d7aaf7a2306b9c922fb8c48a2f5d318
qasim <qrafique@gmail.com>
2024-10-13 21:38:02 +0500

    forloop list iterate and print 1-10

--- loops.py
+++ loops.py
@@ -6,3 +6,15 @@ list_of_cloud.append('IBM')
 print(list_of_cloud)
 list_of_cloud.insert(1,'orwcle')
 print(list_of_cloud)
+
+# count no of elements in list
+
+print(len(list_of_cloud))
+
+# iterate list with the help of loop
+
+for cloud in list_of_cloud:
+    print(cloud)
+
+for i in range(0,10):
+    print(i+1)
