1761c7782ac0532ebe31c0256e389d11bd797b93
qasim <qrafique@gmail.com>
2024-10-13 20:27:23 +0500

    condiional.py calculatr code added

--- conditional.py
+++ conditional.py
@@ -14,3 +14,28 @@ if day_of_week =='saturday' or day_of_week=='sunday':
     print("i will learn devOps live!")
 else:
     print(" iwill practice devOps!!!!!!")
+
+
+#calculator with coditional statemnt
+
+num1= int(input("enter no.1 "))
+num2=int(input("enter num2:"))
+operation=input("choose option( '+','-','*','/','%')")
+if operation=='+':
+    sum=num1+num2
+    print("addition:",sum)
+elif operation=='-':
+    diff=num1-num2
+    print("subtraction:", sub)
+
+elif operation=='*':
+    product=num1*num2
+    print("product:", product)
+elif operation=='/':
+    div=num1/num2
+    print("division:",div)
+elif operation=='%':
+    rem=num1%num2
+    print("remainder is:",rem)
+else:
+    print("invalid option")
