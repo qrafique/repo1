72ac59294581900f231361bb14244c004db76595
qasim <qrafique@gmail.com>
2024-10-13 21:27:36 +0500

    listappend-insert

---
+++ loops.py
@@ -0,0 +1,8 @@
+#list is data structure to add multipe values . these multiple
+# can be same or different data types.
+list_of_cloud=['gcp', 'aws','digitalOcean','azure']
+print(list_of_cloud)
+list_of_cloud.append('IBM')
+print(list_of_cloud)
+list_of_cloud.insert(1,'orwcle')
+print(list_of_cloud)
