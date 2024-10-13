1d92c5cca2bd22ffbc4231ac7b99b41fc16b55df
qasim <qrafique@gmail.com>
2024-10-13 22:23:32 +0500

    some odd

--- utils.py
+++ utils.py
@@ -1,3 +1,19 @@
-import os # importing library into code about operating system
+import datetime
+print(datetime.datetime.today())
 
-print(os.system('df -h'))
+import os
+print(os.system('date'))
+
+
+
+cmd = "git --version"
+
+returned_value = os.system(cmd)  # returns the exit code in unix
+print('returned value:', returned_value)
+
+import subprocess
+
+cmd = "git --version"
+
+returned_value = subprocess.call(cmd, shell=True)  # returns the exit code in unix
+print('returned value:', returned_value)
